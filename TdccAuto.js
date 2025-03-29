64262c8ca95298c9ce44cef6daa8ed5f3a42a1fe
owen <12877999+zxc88645@users.noreply.github.com>
2025-03-29 21:07:38 +0800

    從"全部同意" 修改成 "全部棄權"

--- TdccAuto.js
+++ TdccAuto.js
@@ -9,7 +9,6 @@
 // @grant        none
 // @require      https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js
-// @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @license      MIT
 // @homepage     https://github.com/zxc88645/TdccAuto
 // @downloadURL  https://update.greasyfork.org/scripts/530187/%E9%9B%BB%E5%AD%90%E6%8A%95%E7%A5%A8%E8%87%AA%E5%8B%95%E6%8A%95%E7%A5%A8.user.js
@@ -100,14 +99,18 @@
         if (currentPath.includes('/evote/shareholder/001/')) {
             console.log('進行電子投票');
 
-            await clickAndWait('body > div.c-main > form > table:nth-child(3) > tbody > tr.u-t_align--right > td:nth-child(2) > a:nth-child(1)', '全部贊成(承認)', '步驟 1');
-            await clickAndWait('body > div.c-main > form > table:nth-child(4) > tbody > tr:nth-child(2) > td.u-width--50.u-t_align--right > a:nth-child(1)', '全部贊成(承認)', '步驟 2');
-            await clickAndWait('#voteform > table:nth-child(5) > tbody > tr > td.u-t_align--right > label', '全部勾選', '步驟 3');
-            await clickAndWait('#voteform > table:nth-child(5) > tbody > tr > td.u-t_align--right > a:nth-child(5)', '平均分配', '步驟 4');
-            await clickAndWait('body > div.c-main > form > div.c-votelist_actions > button:nth-child(2)', '下一步', '步驟 5');
-            await clickAndWait('#voteform > div.c-votelist_actions > button:nth-child(1)', '下一步', '步驟 6');
-            await clickAndWait('body > div.c-main > form > div.c-votelist_actions > button:nth-child(1)', '確認投票結果', '步驟 7');
-            await clickAndWait('#go', '確認', '步驟 8');
+            // 全部棄權
+            await clickAndWait('body > div.c-main > form > table:nth-child(3) > tbody > tr.u-t_align--right > td:nth-child(2) > a:nth-child(3)', '全部棄權', '勾選全部棄權(1)');
+            await clickAndWait('body > div.c-main > form > div.c-votelist_actions > button:nth-child(2)', '下一步', '按下 下一步(1)');
+
+            // 全部棄權 2
+            await clickAndWait('#voteform > table:nth-child(5) > tbody > tr > td.u-t_align--right > a:nth-child(8)', '全部棄權', '勾選全部棄權(2)');
+            await clickAndWait('#voteform > div.c-votelist_actions > button:nth-child(1)', '下一步', '按下 下一步(2)');
+            await clickAndWait('body > div.jquery-modal.blocker.current > div > div:nth-child(2) > button:nth-child(1)', '下一步', '按下 下一步(2.2)');
+
+            // 確認投票結果
+            await clickAndWait('body > div.c-main > form > div.c-votelist_actions > button:nth-child(1)', '確認投票結果', '確認投票結果');
+
 
         } else if (currentPath === '/evote/shareholder/000/tc_estock_welshas.html') {
             console.log('位於投票列表首頁');
